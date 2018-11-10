3ac79dcec42704818c0ba2cdba381a167d4f1d7b
Alejandro Endo <alejoendo@hotmail.com>
2018-11-10 10:34:37 -0500

    Initial Commit

---
+++ src/exe/Main.java
@@ -0,0 +1,29 @@
+package exe;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("exe.Main");
+	}
+	
+	@Override
+	public void settings() {
+		// TODO Auto-generated method stub
+		super.settings();
+	}
+	
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		super.draw();
+	}
+
+}
